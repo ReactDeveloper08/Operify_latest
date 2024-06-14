7a501d88d7dd884f1dbf273c3742970bb47dbdf7
Doomshell2022 <app@doomshell.com>
2024-06-14 10:58:31 +0530

    kh

--- ios/OperifyTests/OperifyTests.m
+++ ios/OperifyTests/OperifyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tpplTests : XCTestCase
+@interface OperifyTests : XCTestCase
 
 @end
 
-@implementation tpplTests
+@implementation OperifyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
